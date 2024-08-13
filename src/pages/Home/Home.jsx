68cd8e903efa833d086ad78144b16292d3e15d42
shahin2525 <shahin451999@gmail.com>
2024-08-12 21:54:39 -0700

    add moment js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,25 @@
+import Header from "../shared/Header/Header";
+import LeftSideNav from "../shared/LeftSideNav/LeftSideNav";
+import Navbar from "../shared/Navbar/Navbar";
+import RightSideNav from "../shared/RightSideNav/RightSideNav";
+
 const Home = () => {
   return (
-    <div>
-      <h1 className="font-poppins">i am from home</h1>
-    </div>
+    <>
+      <Header></Header>
+      <Navbar></Navbar>
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-5">
+        <div className="border">
+          <LeftSideNav></LeftSideNav>
+        </div>
+        <div className="col-span-2 border">
+          <h1>news coming soon</h1>
+        </div>
+        <div className="border">
+          <RightSideNav></RightSideNav>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,7 +1,9 @@
+import moment from "moment";
+
 const Header = () => {
   return (
     <div>
-      <h1>header component</h1>
+      <h1>{moment().format("dddd MMMM DD YYYY")}</h1>
     </div>
   );
 };
