fc045a9c9b2b6af0b406dee39280e486922ecf95
shahin2525 <shahin451999@gmail.com>
2024-08-14 13:27:08 -0700

    add login page

---
+++ src/pages/components/Login/Login.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+import Navbar from "../../shared/Navbar/Navbar";
+
+const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const password = form.get("password");
+    console.log(password);
+  };
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1 className="text-center">please login </h1>
+      <form
+        onSubmit={handleLogin}
+        className="card-body md:w-3/4 lg:w-1/2 mx-auto"
+      >
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="email"
+            className="input input-bordered"
+            required
+            name="email"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="password"
+            className="input input-bordered"
+            required
+            name="password"
+          />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">
+              Forgot password?
+            </a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+      <p className="text-center">
+        Do not have an account{" "}
+        <Link className="text-blue-500 text-bold" to="/register">
+          Register
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
