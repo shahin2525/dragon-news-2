58535e672af9982885efa414dab45f508f56ee4e
shahin2525 <shahin451999@gmail.com>
2024-08-13 23:36:52 -0700

    add leftside nav

--- src/pages/shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+  console.log(categories);
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
   return (
     <div>
-      <h1>left side nav</h1>
+      {categories.map((category) => (
+        <Link className="block space-y-2 p-2" key={category.id}>
+          {category.name}
+        </Link>
+      ))}
     </div>
   );
 };
