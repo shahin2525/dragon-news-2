f56ab943ea82cad42fe3c70ba6aa4578033ad537
shahin2525 <shahin451999@gmail.com>
2024-08-18 00:42:18 -0700

    add env file

--- src/firebase/firebase_init.js
+++ src/firebase/firebase_init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCspKRskY47XhRtwZDoxkNIe4eiauA4TTo",
-  authDomain: "simple-firebase-p-1d82b.firebaseapp.com",
-  projectId: "simple-firebase-p-1d82b",
-  storageBucket: "simple-firebase-p-1d82b.appspot.com",
-  messagingSenderId: "805341972095",
-  appId: "1:805341972095:web:f2e2fca68db6da0e90a14a",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
